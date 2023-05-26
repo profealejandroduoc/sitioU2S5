d41e998ef0f541dad6321fb68df1688317dd292b
profealejandroduoc <al.sepulvedam@profesor.duoc.cl>
2023-05-26 09:14:58 -0400

    Eliminadas las vistas

--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,16 +1,3 @@
 from django.shortcuts import render
 from datetime import date
 
-# Create your views here.
-def index(request):
-    return render(request,"aplicacion/index.html")
-
-
-def paginita(request):
-    fecha=date.today()
-    
-    context={
-        "date":fecha
-    }
-
-    return render(request,"aplicacion/paginita.html",context)
