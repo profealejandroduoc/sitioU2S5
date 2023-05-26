1d3ebecf654ba88a0515b5896ca5f05c97b71fef
profealejandroduoc <al.sepulvedam@profesor.duoc.cl>
2023-05-26 10:36:20 -0400

    Listado de personas y base listo

--- aplicacion/urls.py
+++ aplicacion/urls.py
@@ -1,9 +1,10 @@
 #URLS.py aplicacion
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index,personas
 
 urlpatterns=[
     path('',index,name="index"),
+    path('personas',personas,name="personas"),
 
 ]
--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from datetime import date
+from .models import Persona,Mascota
 
 def index(request):
     return render(request,'aplicacion/index.html')
+
+def personas(request):
+    fecha=date.today()
+    autor="El profe"
+    people=Persona.objects.all()
+
+    contexto={
+        "fecha":fecha,
+        "autor":autor,
+        "personas":people
+    }
+    return render(request,'aplicacion/personas.html',contexto)
